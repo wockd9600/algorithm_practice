eab6145382186e71886b6d911c0c6c6b973e4537
wockd9600 <68463308+wockd9600@users.noreply.github.com>
2021-01-12 00:04:15 +0900

    Update 2 부품 찾기(set).py

--- 백준/set()의 활용/2 부품 찾기(set).py
+++ 백준/set()의 활용/2 부품 찾기(set).py
@@ -10,3 +10,11 @@ for m in materialM:
         print("yes", end=' ')
     else:
         print("no", end=' ')
+
+"""
+Set
+파이썬에서 탐색할 땐 set함수를 활용할 수 있다.
+set은 정렬이란 개념이 없기 때문에 index를 사용하지 않는다. 또한 수학의 집합 개념이라고 생각할 수 있고 중복이 없다.
+해시 테이블로알려진 데이터 구조를 기반으로 한다.
+참고 : https://www.geeksforgeeks.org/sets-in-python/
+"""

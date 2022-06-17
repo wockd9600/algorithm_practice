e00b6040c8bd8f9036545ba9f2d1b7dddf68426d
JaeChang <wockd9600@naver.com>
2022-06-17 12:03:05 +0900

    baekjoon 14916

--- 백준/풀이/14916.py
+++ 백준/풀이/14916.py
@@ -1,4 +1,4 @@
-INF = int(10e9)
+INF = 1000000
 
 n = int(input())
 dp = [INF] * (6)
@@ -7,4 +7,4 @@ dp[2], dp[4], dp[5] = 1, 2, 1
 for i in range(6, n + 1):
     dp.append(min(dp[i-2], dp[i-5]) + 1)
 
-print(dp[n])
+print(dp[n] if dp[n] < INF else -1)

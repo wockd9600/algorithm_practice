dfe0f0b38b4a67ba120f8f35486768b49de93264
JaeChang <wockd9600@naver.com>
2022-06-20 15:43:49 +0900

    baekjoon 1965

---
+++ 백준/풀이/1965.py
@@ -0,0 +1,16 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+arr = list(map(int, input().split()))
+dp = [0] * n
+
+for i in range(n):
+    s = [0]
+    for j in range(i, -1, -1):
+        if arr[i] > arr[j]:
+            s.append(dp[j])
+    
+    dp[i] = max(s) + 1
+
+print(max(dp))
